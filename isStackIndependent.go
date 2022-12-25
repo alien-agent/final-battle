41f29250bc93e9570edbb6e4de2b046d57858528
Kekega <saikin.ao@phystech.edu>
2022-12-25 23:03:48 +0300

    stack

--- isStackIndependent.go
+++ isStackIndependent.go
@@ -1,11 +1,10 @@
 package main
 
+// IsStackIndependent returns whether the transition is stack independent or not
 func (t *Transition) IsStackIndependent(pda *PDA) bool {
 
-	for _, transition := range pda.Transitions {
-		if transition.Pop == string(UniversalQuantifier) {
-			return true
-		}
+	if t.Pop == string(UniversalQuantifier) {
+		return true
 	}
 
 	numTransitions := 0
