70cd9b2841c278d8ddab8ca75bc70a1d7eca6746
Kekega <saikin.ao@phystech.edu>
2022-12-29 21:29:53 +0300

    revert

--- predicates.go
+++ predicates.go
@@ -1,52 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"golang.org/x/exp/slices"
-)
+import "golang.org/x/exp/slices"
 
-func (p *PDA) IsStackIndependent(t Transition) bool {
+func (p *PDA) IsStackIndependent(t *Transition) bool {
 	if t.Pop == string(UniversalQuantifier) {
 		return true
 	}
 
-	var array2 []string
+	numTransitions := 0
 	for _, transition := range p.Transitions {
 		if transition.From == t.From && transition.To == t.To {
-			array2 = append(array2, transition.Pop)
+			numTransitions++
 		}
 	}
-
-
-	// Create a map to store the elements of array1
-	set1 := make(map[string]struct{})
-	for _, element := range p.StackAlphabet {
-		set1[element] = struct{}{}
-	}
-
-	// Create a map to store the elements of array2
-	set2 := make(map[string]struct{})
-	for _, element := range array2 {
-		set2[element] = struct{}{}
-	}
-
-	// Check if the two sets are equal
-	if len(set1) == len(set2) {
-		equal := true
-		for key := range set1 {
-			if _, ok := set2[key]; !ok {
-				equal = false
-				break
-			}
-		}
-		if equal {
-			return true
-		} else {
-			return false
-		}
-	} else {
-		return false
-	}
+	return numTransitions == len(p.StackAlphabet)
 }
 
 func (p *PDA) IsTransitionDeterministic(t Transition) bool {
